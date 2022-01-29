e76b530d6d41bbeb64bda0577018764ef88c5530
LuminousDefex <defex84@gmail.com>
2022-01-29 19:20:48 +0200

    setting up firebase

---
+++ firebase.js
@@ -0,0 +1,20 @@
+import firebase from 'firebase';
+
+const firebaseConfig = {
+    apiKey: "AIzaSyD95_ml5FiEsiCoEf4dbHcRYNsfINCcjYE",
+    authDomain: "whatsapp-2-dc807.firebaseapp.com",
+    projectId: "whatsapp-2-dc807",
+    storageBucket: "whatsapp-2-dc807.appspot.com",
+    messagingSenderId: "395701135954",
+    appId: "1:395701135954:web:9ba0b2941d7627531b4d8b"
+};
+
+const app = !firebase.apps.length
+    ? firebase.initializeApp(firebaseConfig)
+    : firebase.app();
+
+const db = app.firestore();
+const auth = app.auth();
+const provider = new firebase.auth.GoogleAuthProvier();
+
+export { db, auth, provider }
